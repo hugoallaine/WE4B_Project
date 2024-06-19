294534a3e0e30250eb563fe82e346a8d52d50f7e
balezeauquentin <quentin.balezeau70@gmail.com>
2024-06-19 21:37:50 +0200

    amélioration du player

--- angular-app/src/app/media-player/media-player.component.ts
+++ angular-app/src/app/media-player/media-player.component.ts
@@ -7,18 +7,39 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 })
 export class MediaPlayerComponent {
   @ViewChild('videoPlayer') videoPlayer!: ElementRef;
+  @ViewChild('progressBar') progressBar!: ElementRef;
 
-  playVideo() {
-    this.videoPlayer.nativeElement.play();
+  isPlaying: boolean = false;
+
+  ngAfterViewInit() {
+    this.videoPlayer.nativeElement.addEventListener('timeupdate', this.updateProgressBar.bind(this));
+    this.videoPlayer.nativeElement.addEventListener('play', () => this.isPlaying = true);
+    this.videoPlayer.nativeElement.addEventListener('pause', () => this.isPlaying = false);
+  }
+  
+  updateProgressBar() {
+    const duration = this.videoPlayer.nativeElement.duration;
+    const currentTime = this.videoPlayer.nativeElement.currentTime;
+    if (duration > 0) {
+      this.progressBar.nativeElement.value = (currentTime / duration) * 100;
+    }
   }
 
-  pauseVideo() {
-    this.videoPlayer.nativeElement.pause();
+
+  seek(event: Event) {
+    const seekTo = parseFloat((event.target as HTMLInputElement).value);
+    const duration = this.videoPlayer.nativeElement.duration;
+    if (duration > 0) {
+      this.videoPlayer.nativeElement.currentTime = (seekTo / 100) * duration;
+    }
   }
 
-  stopVideo() {
-    this.videoPlayer.nativeElement.pause();
-    this.videoPlayer.nativeElement.currentTime = 0;
+  togglePlayPause() {
+    if (this.videoPlayer.nativeElement.paused) {
+      this.videoPlayer.nativeElement.play();
+    } else {
+      this.videoPlayer.nativeElement.pause();
+    }
   }
 
   setVolume(event: Event) {
