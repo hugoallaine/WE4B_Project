d1f101d1a4694856c383f53a767d13efc15c2849
zentox <zentoxll@gmail.com>
2024-06-19 19:35:31 +0200

    Update element-cover.component.html to add conditional rendering for different element types

--- angular-app/src/app/models/element.model.ts
+++ angular-app/src/app/models/element.model.ts
@@ -1,7 +1,14 @@
+export enum ElementType {
+    Album = 'album',
+    Music = 'music',
+    Artist = 'artist'
+}
+
 export interface Element {
     id: number;
     name: string;
     coverUrl: string;
     artist: string;
-  }
+    type: ElementType;
+}
   
