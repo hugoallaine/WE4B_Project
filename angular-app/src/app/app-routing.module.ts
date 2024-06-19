dbcbf5114925323d15b4b5e6942a54523b02148a
balezeauquentin <quentin.balezeau70@gmail.com>
2024-06-19 11:18:01 +0200

    Création du media player

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,22 +1,24 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
-import { AuthGuard } from './guards/auth.guard';
-import { ElementCoverComponent } from './element-cover/element-cover.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { HomeComponent } from "./home/home.component";
+import { AuthGuard } from "./guards/auth.guard";
+import { ElementCoverComponent } from "./element-cover/element-cover.component";
+import { MediaPlayerComponent } from "./media-player/media-player.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'cover', component: ElementCoverComponent },
-  { path: '**', redirectTo: 'home' }
+    { path: "", redirectTo: "home", pathMatch: "full" },
+    { path: "home", component: HomeComponent, canActivate: [AuthGuard] },
+    { path: "login", component: LoginComponent },
+    { path: "register", component: RegisterComponent },
+    { path: "cover", component: ElementCoverComponent },
+    { path: "player", component: MediaPlayerComponent },
+    { path: "**", redirectTo: "home" },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,37 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
-import { AccountComponent } from './account/account.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { StyleModule } from './style/style.module';
-import { ElementCoverComponent } from './element-cover/element-cover.component';
-import { AuthGuard } from './guards/auth.guard';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HomeComponent } from "./home/home.component";
+import { AccountComponent } from "./account/account.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { StyleModule } from "./style/style.module";
+import { ElementCoverComponent } from "./element-cover/element-cover.component";
+import { AuthGuard } from "./guards/auth.guard";
+import { MediaPlayerComponent } from "./media-player/media-player.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent,
-    AccountComponent,
-    LoginComponent,
-    RegisterComponent,
-    ElementCoverComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    StyleModule
-  ],
-  providers: [AuthGuard],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        HomeComponent,
+        AccountComponent,
+        LoginComponent,
+        RegisterComponent,
+        ElementCoverComponent,
+        MediaPlayerComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        StyleModule,
+    ],
+    providers: [AuthGuard],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
